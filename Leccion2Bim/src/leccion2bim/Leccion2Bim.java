dbdab93858703ae498c21bcc2f77ec4535408f47
PalaHz <38107718+PalaHz@users.noreply.github.com>
2018-06-05 18:58:28 -0500

    Update Leccion2Bim.java

--- Leccion2Bim/src/leccion2bim/Leccion2Bim.java
+++ Leccion2Bim/src/leccion2bim/Leccion2Bim.java
@@ -7,6 +7,7 @@ public class Leccion2Bim {
         int [] arreglo2 = {10, 20, 30, 40, 50, 60};
         double [] arreglo3 = new double [6];
         double suma = 0;
+        double promedio;
         
         //Imprimir encabezado
         System.out.print("\nArreglo1\tArreglo2\tArreglo3\n");
@@ -17,7 +18,9 @@ public class Leccion2Bim {
             System.out.printf("%d\t\t%d\t\t%.2f\n", arreglo1[contador], arreglo2[contador], arreglo3[contador]);
             suma = suma + arreglo3[contador];
         }
+        promedio = suma/arreglo1.length;
+        
         //Imprimir Promedio de arreglo 3
-        System.out.print("El promedio del arreglo 3 es "+(suma/arreglo1.length+"\n"));
+        System.out.print("El promedio del arreglo 3 es "+promedio+"\n");
     }
 }
