4882c1f4289fdeedc2476bb5e3348fae36e35f89
Pala <andrespala2000@gmail.com>
2018-06-05 18:55:56 -0500

    Leccion extraclase

---
+++ Leccion2Bim/src/leccion2bim/Leccion2Bim.java
@@ -0,0 +1,23 @@
+package leccion2bim;
+public class Leccion2Bim {
+    public static void main(String[] args) {
+        
+        //Inicializacion - Declaración de variables
+        int [] arreglo1 = {110, 220, 430, 140, 250, 460};
+        int [] arreglo2 = {10, 20, 30, 40, 50, 60};
+        double [] arreglo3 = new double [6];
+        double suma = 0;
+        
+        //Imprimir encabezado
+        System.out.print("\nArreglo1\tArreglo2\tArreglo3\n");
+        
+        // Calculo de valores para el arreglo 3 - Impresión de valores
+        for (int contador = 0; contador < arreglo1.length; contador++){
+            arreglo3[contador] = arreglo1[contador] / arreglo2[contador];
+            System.out.printf("%d\t\t%d\t\t%.2f\n", arreglo1[contador], arreglo2[contador], arreglo3[contador]);
+            suma = suma + arreglo3[contador];
+        }
+        //Imprimir Promedio de arreglo 3
+        System.out.print("El promedio del arreglo 3 es "+(suma/arreglo1.length+"\n"));
+    }
+}
